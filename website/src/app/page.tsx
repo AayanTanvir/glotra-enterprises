02df0d98fff6fe2afa2589d379a1ff9c6481a869
AayanTanvir <k98778376@gmail.com>
2025-07-24 14:37:46 +0500

    Finish: Why Choose Us + Industries We Serve Sections

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -3,11 +3,65 @@
 import React from 'react';
 import Button from '@/components/Button';
 import Image from 'next/image';
-import { BsGlobe } from "react-icons/bs";
-import { FaRegHandshake } from "react-icons/fa";
-import { RiMenuSearchLine, RiTeamLine } from "react-icons/ri";
+import { BiWorld } from "react-icons/bi";
+import { FiMessageCircle } from "react-icons/fi";
+import { IoShieldCheckmarkOutline } from "react-icons/io5";
+import { MdWorkOutline } from "react-icons/md";
+import { LuWheat } from "react-icons/lu";
+import { HiOutlineBolt } from "react-icons/hi2";
+import { RiOilLine } from "react-icons/ri";
+import { MdOutlineFactory } from "react-icons/md";
+import { PiTestTube } from "react-icons/pi";
 
 export default function Home() {
+
+
+    const whyUsPoints = [
+        {
+            icon: <BiWorld className='text-foreground text-2xl xs:text-3xl' />,
+            title: 'Global Partnerships',
+            description: 'We work with trusted manufacturers worldwide to help businesses source products across various sectors with confidence.'
+        },
+        {
+            icon: <MdWorkOutline className='text-foreground text-2xl xs:text-3xl' />,
+            title: 'Years of Experience',
+            description: 'We manage logistics efficiently to ensure timely delivery of goods.'
+        },
+        {
+            icon: <IoShieldCheckmarkOutline className='text-foreground text-2xl xs:text-3xl' />,
+            title: 'Quality Assurance',
+            description: 'We ensure the highest standards of quality in all our products and services.'
+        },
+        {
+            icon: <FiMessageCircle className='text-foreground text-2xl xs:text-3xl' />,
+            title: 'Transparent Communication',
+            description: 'We keep you informed at every step, from inquiry to delivery.'
+        },
+    ]
+
+    const industries = [
+        {
+            icon: <LuWheat className='text-foreground text-2xl xs:text-3xl' />,
+            title: 'Agriculture',
+            color: 'gold',
+        },
+        {
+            icon: <HiOutlineBolt className='text-foreground text-2xl xs:text-3xl' />,
+            title: 'Energy',
+        },
+        {
+            icon: <RiOilLine className='text-foreground text-2xl xs:text-3xl' />,
+            title: 'Oil & Gas',
+        },
+        {
+            icon: <MdOutlineFactory className='text-foreground text-2xl xs:text-3xl' />,
+            title: 'Manufacturing',
+        },
+        {
+            icon: <PiTestTube className='text-foreground text-2xl xs:text-3xl' />,
+            title: 'Chemicals',
+        }
+    ]
 
     return (
         <div className="w-full h-fit flex justify-start items-center flex-col mt-8 md:mt-16">
@@ -26,46 +80,35 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <div className='w-4/5 h-[1px] rounded-full bg-foreground opacity-20 my-12 '/>
-            <div className='w-full h-fit flex justify-start items-center flex-col px-4'>
-                <h2 className='font-geist text-2xl sm:text-3xl xl:text-4xl font-semibold mb-4 xl:mb-6 select-none'>Our Services</h2>
-                <div className='w-full md:w-4/5 h-fit grid grid-cols-1 md:grid-cols-2 gap-4'>
-                    <div className='w-full h-full rounded-lg border-[1px] border-foreground flex justify-normal items-center '>
-                        <div className='w-12 xl:w-16 h-full flex justify-center items-center'>
-                            <FaRegHandshake className='text-foreground text-2xl lg:text-3xl xl:text-4xl' />
+            <div className='w-3/5 h-[1px] rounded-full bg-foreground/10 my-12 '/>
+            <div className='w-full h-fit flex justify-start items-center flex-col px-4 mb-12'>
+                <h2 className='font-geist text-2xl sm:text-3xl xl:text-4xl font-semibold mb-4 xl:mb-6 select-none italic'>Why Choose <span className='text-primary-300'>Us</span>?</h2>
+                <div className='w-full sm:w-4/5 h-fit flex flex-col justify-start items-start gap-6'>
+                    {whyUsPoints.map((point, index) => (
+                        <div className='w-full sm:w-4/5 h-fit flex justify-start items-center' key={index}>
+                            <div className='w-12 h-12 flex justify-center items-center border-[1px] border-foreground/30 rounded-full'>
+                                {point.icon}
+                            </div>
+                            <div className='flex-1 flex flex-col justify-start items-start pl-4 gap-1'>
+                                <h3 className='font-geist text-base xl:text-xl font-semibold select-none'>{point.title}</h3>
+                                <p className='font-geist text-xs xl:text-base select-none'>{point.description}</p>
+                            </div>
                         </div>
-                        <div className='flex-1 flex flex-col justify-start items-start gap-1 p-2 lg:py-4'>
-                            <h3 className='font-geist text-base xl:text-xl font-semibold select-none'>Indenting</h3>
-                            <p className='font-geist text-xs xl:text-base leading-4.5 select-none'>We act as a trusted bridge between buyers and international suppliers, ensuring smooth trade deals from start to finish.</p>
+                    ))}
+                </div>
+            </div>
+            <div className='w-4/5 xl:w-[70%] h-fit flex justify-between items-center flex-col sm:flex-row px-4 mt-12 sm:gap-10'>
+                <div className='h-full flex justify-center items-center flex-col sm:items-start mb-4 xl:mb-6'>
+                    <h2 className='font-geist text-2xl text-center sm:text-3xl xl:text-4xl font-semibold select-none'>Industries <span className='text-primary-300'>We</span> Serve</h2>
+                    <p className='hidden sm:block font-geist text-xs xl:text-base select-none'>We work with businesses in the following industries and others</p>
+                </div>
+                <div className='w-full sm:w-fit grid grid-cols-1 md:grid-cols-2 gap-4'>
+                    {industries.map((industry, index) => (
+                        <div key={index} className={`flex justify-center items-center gap-2 sm:p-2 sm:border-[1px] sm:border-foreground/30 sm:rounded-lg hover:bg-primary-300/5 transition-all duration-200 ease-in-out`}>
+                            {industry.icon}
+                            <h3 className='font-geist text-base xl:text-xl font-semibold select-none'>{industry.title}</h3>
                         </div>
-                    </div>
-                    <div className='w-full h-full rounded-lg border-[1px] border-foreground flex justify-normal items-center '>
-                        <div className='w-12 xl:w-16 h-full flex justify-center items-center'>
-                            <RiTeamLine className='text-foreground text-2xl lg:text-3xl xl:text-4xl' />
-                        </div>
-                        <div className='flex-1 flex flex-col justify-start items-start gap-1 p-2 lg:py-4'>
-                            <h3 className='font-geist text-base xl:text-xl font-semibold select-none'>Trading Consultation</h3>
-                            <p className='font-geist text-xs xl:text-base leading-4.5 select-none'>We offer comprehensive trading consultation services to help you navigate the global market.</p>
-                        </div>
-                    </div>
-                    <div className='w-full h-full rounded-lg border-[1px] border-foreground flex justify-normal items-center '>
-                        <div className='w-12 xl:w-16 h-full flex justify-center items-center'>
-                            <BsGlobe className='text-foreground text-2xl lg:text-3xl xl:text-4xl' />
-                        </div>
-                        <div className='flex-1 flex flex-col justify-start items-start gap-1 p-2 lg:py-4'>
-                            <h3 className='font-geist text-base xl:text-xl font-semibold select-none'>Import/Export</h3>
-                            <p className='font-geist text-xs xl:text-base leading-4.5 select-none'>We handle global shipping and documentation, helping you import or export products efficiently and legally.</p>
-                        </div>
-                    </div>
-                    <div className='w-full h-full rounded-lg border-[1px] border-foreground flex justify-normal items-center '>
-                        <div className='w-12 xl:w-16 h-full flex justify-center items-center'>
-                            <RiMenuSearchLine className='text-foreground text-2xl lg:text-3xl xl:text-4xl' />
-                        </div>
-                        <div className='flex-1 flex flex-col justify-start items-start gap-1 p-2 lg:py-4'>
-                            <h3 className='font-geist text-base xl:text-xl font-semibold select-none'>Product Sourcing</h3>
-                            <p className='font-geist text-xs xl:text-base leading-4.5 select-none'>We provide dedicated team support services to help you achieve your business goals.</p>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
--- website/src/components/Navbar.tsx
+++ website/src/components/Navbar.tsx
@@ -11,10 +11,11 @@ export default function Navbar() {
                 <Image src="/glotra_logo.svg" alt="Glotra Logo" width={30} height={30} />
                 <h1 className='hidden sm:block font-rubik text-2xl font-semibold'>Glotra</h1>
             </Link>
-            <nav className='w-fit h-full flex justify-between items-center gap-3 sm:gap-4'>
-                <Navlink to="/about" className='text-xs sm:text-base'>About Us</Navlink>
-                <Navlink to="/services" className='text-xs sm:text-base'>Services</Navlink>
-                <Navlink to="/contact" className='text-xs sm:text-base'>Contact Us</Navlink>
+            <nav className='w-fit h-full flex justify-between items-center gap-2 sm:gap-4'>
+                <Navlink to="/about" className='text-xs text-nowrap sm:text-base'>About Us</Navlink>
+                <Navlink to="/products" className='text-xs text-nowrap sm:text-base'>Products</Navlink>
+                <Navlink to="/services" className='text-xs text-nowrap sm:text-base'>Services</Navlink>
+                <Navlink to="/contact" className='text-xs text-nowrap sm:text-base'>Contact Us</Navlink>
             </nav>
         </div>
     )
